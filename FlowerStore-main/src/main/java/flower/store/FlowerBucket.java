32b4c3e6374d24067ba95fa2a51d16f8a26429fb
Andrii <146644879+bilyi1andrii@users.noreply.github.com>
2024-10-13 20:31:07 +0300

    Add files via upload

---
+++ FlowerStore-main/src/main/java/flower/store/FlowerBucket.java
@@ -0,0 +1,24 @@
+package flower.store;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FlowerBucket {
+    private List<FlowerPack> flowerPacks;
+
+    public FlowerBucket() {
+        flowerPacks = new ArrayList<>();
+    }
+
+    public void addFlowerPack(FlowerPack flowerPack) {
+        flowerPacks.add(flowerPack);
+    }
+
+    public double getPrice() {
+        double price = 0;
+        for (FlowerPack flowerPack: flowerPacks) {
+            price += flowerPack.getPrice();
+        }
+        return price;
+    }
+}
---
+++ FlowerStore-main/src/main/java/flower/store/FlowerPack.java
@@ -0,0 +1,19 @@
+package flower.store;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class FlowerPack {
+    private Flower flower;
+    private int count;
+
+    public FlowerPack(Flower flower, int count) {
+        this.flower = new Flower(flower);
+        this.count = count;
+    }
+
+    public double getPrice() {
+        return flower.getPrice() * count;
+    }
+}
